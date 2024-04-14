2644336a8b56af5d7fcc6252aa668368697d8686
John-Dembaremba <johndembaremba97@gmail.com>
2024-04-14 22:45:54 +0200

    shape implementation

---
+++ shape/shape.go
@@ -0,0 +1,33 @@
+package shape
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Length float64
+	Width  float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
+}
