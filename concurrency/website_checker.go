693e3a6be6e8d57d1534c2b88756d26842dcd660
John-Dembaremba <johndembaremba97@gmail.com>
2024-04-29 23:01:33 +0200

    concurrency

---
+++ concurrency/website_checker.go
@@ -0,0 +1,25 @@
+package concurrency
+
+type WebSiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
+
+func CheckWebsite(wc WebSiteChecker, urls []string) map[string]bool {
+	resultChannel := make(chan result)
+
+	results := make(map[string]bool)
+	for _, url := range urls {
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
+	return results
+}
